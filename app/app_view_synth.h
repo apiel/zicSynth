5c8c65597753cbb34c029cb1642f8ab2e3ddb2e4
Alexandre Piel <Alexandre.Piel@zooplus.com>
2022-10-11 17:23:35 +0200

    wip synth

--- app/app_view_synth.h
+++ app/app_view_synth.h
@@ -2,18 +2,51 @@
 #define APP_VIEW_SYNTH_H_
 
 #include "./app_view.h"
+#include "./app_view_table.h"
 
-class App_View_Synth : public App_View {
-protected:
+#define VIEW_INSTR_ROW 8
+#define VIEW_INSTR_COL 3
+
+class App_View_SynthRow : public App_View_TableLabeledRow {
 public:
-    void render(App_Renderer* renderer)
+    App_View_SynthRow()
+        : App_View_TableLabeledRow("Hello ", 2)
     {
-        strcpy(renderer->text, "Hello synth");
     }
 
-    uint8_t update(UiKeys* keys, App_Renderer* renderer)
+    void renderValue(App_Renderer* renderer, uint8_t col)
+    {
+        // sprintf(renderer->text + strlen(renderer->text), "%-2d", tracks->trackId + 1);
+        strcat(renderer->text, "World");
+    }
+
+    uint8_t update(UiKeys* keys, App_Renderer* renderer, uint8_t row, uint8_t col) override
     {
         return VIEW_NONE;
+    }
+};
+
+class App_View_Synth : public App_View_Table {
+protected:
+    App_View_SynthRow synthField;
+
+    App_View_TableField* fields[VIEW_INSTR_ROW * VIEW_INSTR_COL] = {
+        // clang-format off
+        &synthField, &synthField, NULL,
+        NULL, NULL, NULL,
+        NULL, NULL, NULL,
+        NULL, NULL, NULL,
+        NULL, NULL, NULL,
+        NULL, NULL, NULL,
+        NULL, NULL, NULL,
+        NULL, NULL, NULL,
+        // clang-format on
+    };
+
+public:
+    App_View_Synth()
+        : App_View_Table(fields, VIEW_INSTR_ROW, VIEW_INSTR_COL)
+    {
     }
 };
 
