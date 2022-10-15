99c29ecacc11fbc92b53acd3aa325660bc883d64
Alexandre Piel <Alexandre.Piel@zooplus.com>
2022-10-15 20:30:43 +0200

    open default wavetable

--- app/app_synth.h
+++ app/app_synth.h
@@ -3,13 +3,26 @@
 
 #include <zic_mod_filter.h>
 #include <zic_wave_file.h>
+#include <app_core_file.h>
 
 #define APP_SYNTH_WAVE_COUNT 4
+#define APP_WAVETABLES_FOLDER "wavetables"
 
 class App_Synth {
 public:
     Zic_Wave_File wave[APP_SYNTH_WAVE_COUNT];
     Zic_Mod_Filter filter;
+    char filename[256] = "";
+
+    App_Synth()
+    {
+        firstFile(filename, APP_WAVETABLES_FOLDER);
+        for (int i = 0; i < APP_SYNTH_WAVE_COUNT; i++) {
+            char filepath[270];
+            snprintf(filepath, 270, "%s/%s", APP_WAVETABLES_FOLDER, filename);
+            wave[i].open(filepath, true);
+        }
+    }
 
     int16_t sample()
     {
