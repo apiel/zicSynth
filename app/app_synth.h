0dd97d992613f43a3fbb77094efcda181119ada4
Alexandre Piel <Alexandre.Piel@zooplus.com>
2022-10-23 19:53:05 +0200

    cleanup

--- app/app_synth.h
+++ app/app_synth.h
@@ -19,5 +19,9 @@ public:
 };
 
 // the mixer should be done in the part while assigning patches
+//
+// osc in lfo mode could be run in one shot, for example for kick design
+// therefor would need a way to design custom wavetable :p
+// maybe we should think to use linear interpolation for the wavetable to have smoother transitions
 
 #endif
